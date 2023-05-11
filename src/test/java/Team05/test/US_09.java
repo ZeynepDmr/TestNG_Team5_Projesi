7595bbf06fab4fa26967a06f6319db73ad1996ce
Beyza Gulumser <beyzagulumser@gmail.com>
2023-05-11 18:38:39 +0300

    v3

--- src/test/java/Team05/test/US_09.java
+++ src/test/java/Team05/test/US_09.java
@@ -4,7 +4,7 @@ public class US_09 {
     public static void main(String[] args) {
         System.out.println("beyz");
         //madde bir git add .
-        //git commit
+        //git commit -m""
         // git branch: hangi branchte oldugumu gösterir ayrıca branch listemi de gösterir
         //git branch (branch ismi): olusturmak istedigim branchtir
         //git checkout (branch ismi): hangi branche geçmek istersem bunu kullanırız
