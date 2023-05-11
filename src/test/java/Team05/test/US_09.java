c87ba7f430a0e348c4fac11598ae662bbd706f2a
Beyza Gulumser <beyzagulumser@gmail.com>
2023-05-11 18:34:33 +0300

    v2

--- src/test/java/Team05/test/US_09.java
+++ src/test/java/Team05/test/US_09.java
@@ -3,5 +3,15 @@ package Team05.test;
 public class US_09 {
     public static void main(String[] args) {
         System.out.println("beyz");
+        //madde bir git add .
+        //git commit
+        // git branch: hangi branchte oldugumu gösterir ayrıca branch listemi de gösterir
+        //git branch (branch ismi): olusturmak istedigim branchtir
+        //git checkout (branch ismi): hangi branche geçmek istersem bunu kullanırız
+        //git merge branch ismi: branchimi merge yapmak (birlestirmek) istedigimde kullanırım.
+        // master ve maindeyken merge yapmamak gerekli
+
+//branch değişikliği
+
     }
 }
