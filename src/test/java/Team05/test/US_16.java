6c01d0c6e1bb17c9c9140171d704b67347f25da3
Zeynep Demir <zeynepnevadmr@gmail.com>
2023-05-13 20:54:40 +0300

    mesajim

--- src/test/java/Team05/test/US_16.java
+++ src/test/java/Team05/test/US_16.java
@@ -1,9 +1,33 @@
 package Team05.test;
 
+import Team05.utilities.ConfigReader;
+import Team05.utilities.Driver;
+import org.testng.annotations.Test;
+
 public class US_16 {
 
-    public static void main(String[] args) {
-        System.out.println("deniyoruz");
-        System.out.println();
+           /*
+Kullanici url'e gider
+Kullanici sig in butonuna tiklar
+Kullanici gecerli email ve password girer
+Kullanici my account yazisini goruntuler
+Kullanici store manager menusune tiklar
+Kullanici store manager sayfasina gider
+Kullanici products menusune tiklar
+Simple Product default olarak gelmeli
+Virtual ve Downloadable seçilebilmeli
+Price ve Sale Price yazılabilmeli
+US14 ve US15 deki işlemler yapılabilmeli
+Ürünün eklendiğini Product kısmında görebilmeli
+
+            */
+
+    @Test
+    public void test1() {
+
+        //  Kullanici "https://hubcomfy.com" adresine gider
+        Driver.getDriver().get(ConfigReader.getProperty("Hubcomfy_Url"));
+
     }
 }
+
