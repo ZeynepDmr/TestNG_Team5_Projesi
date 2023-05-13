5f8479e3b811ae550c4931b6298dcb542b1e5122
techproed <sunbaharergen@hotmail.com>
2023-05-13 22:59:32 +0200

    v2

--- src/test/java/Team05/pages/BillingUsLocates.java
+++ src/test/java/Team05/pages/BillingUsLocates.java
@@ -26,8 +26,29 @@ public class BillingUsLocates {
        @FindBy(xpath = "//a[@ href='https://hubcomfy.com/my-account-2/edit-address/']")
        public WebElement addresses;
 
-       @FindBy(id = "billing_first_name")
-       public WebElement isim;
+
+       @FindBy(xpath = "(//a[@class='edit btn btn-link btn-primary btn-underline mb-4'])[1]")
+       public WebElement addKutusu;
+
+      @FindBy(xpath = "//input[@id='billing_first_name']")
+      public WebElement firstName;
+
+       @FindBy(id ="billing_last_name")
+       public WebElement lastName;
+
+       @FindBy(xpath ="//input[@name='billing_company']")
+      public  WebElement companyKutusu;
+
+       @FindBy(id ="select2-billing_country-container")
+       public WebElement countrDdm;
+
+       @FindBy(xpath ="//input[@name='billing_address_1']")
+       public WebElement houseNstreetName;
+
+       @FindBy(xpath ="//span[@tittle='Nederlands']")
+       public WebElement nederlandsddm;
+
+
 
 }
 
--- src/test/java/Team05/test/US_03.java
+++ src/test/java/Team05/test/US_03.java
@@ -3,20 +3,25 @@ package Team05.test;
 import Team05.pages.BillingUsLocates;
 import Team05.utilities.ConfigReader;
 import Team05.utilities.Driver;
+import Team05.utilities.ReusableMethods;
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.Test;
+
+import javax.swing.*;
 
 public class US_03 {
 
    /*
 
-    Kullanici Billing Adress bolumunde Add kutusunu tiklar
-    Kullanici First Name kutusuna tiklar
-    Kullanici Isim bilgisini girer
-    Kullanici Last Name kutusuna tiklar
-    Kullanici soyisim bilgisini girer
-    Kullanici Country/Region alanina tiklar
-    Kullanici Country kutucugundan ulke secer
+
+
+
+
+
     Kullanici Street adress alanindaki House number street name alanina tiklar
     Kullanici house number ve street name bilgisini girer
     Kullanici Town/City kutusuna tiklar ve bilgi girisi yapar
@@ -42,16 +47,72 @@ public class US_03 {
         ReusableMethods.bekle(2);
         // Kullanici alt kutucuga passwordu girer
         billingUs.password.sendKeys(ConfigReader.getProperty("Customer_Password"), Keys.ENTER);
-        ReusableMethods.scrollEnd();
         ReusableMethods.bekle(2);
+        ReusableMethods.scrollEnd();
+       // ReusableMethods.bekle(2);
         //Kullanici account kutucuguna tiklar
         Driver.getDriver();
+        ReusableMethods.bekle(2);
         billingUs.myAccount.click();
 
         //Kullanici Addreses bolumune tiklar
         billingUs.addresses.click();
+        ReusableMethods.bekle(2);
 
       //  Kullanici Billing Adress bolumunde Add kutusunu tiklar
+        billingUs.addKutusu.click();
+
+       // Kullanici First Name kutusuna tiklar
+       //  Kullanici Isim bilgisini girer
+        Actions action = new Actions(Driver.getDriver());
+        action.doubleClick(billingUs.firstName);
+        billingUs.firstName.sendKeys("Bahar");
+
+
+       // Kullanici Last Name kutusuna tiklar
+        // Kullanici soyisim bilgisini girer
+        action.doubleClick(billingUs.lastName);
+        billingUs.lastName.sendKeys("'Sonmez");
+
+        action.doubleClick(billingUs.companyKutusu);
+        billingUs.companyKutusu.sendKeys("ABC");
+
+       // Kullanici Country/Region alanina tiklar
+       // Kullanici Country kutucugundan ulke secer
+
+       Driver.getDriver();
+       ReusableMethods.bekle(2);
+       action.sendKeys(Keys.PAGE_DOWN).perform();
+        Driver.getDriver();
+        ReusableMethods.bekle(2);
+      // ReusableMethods.ddmVisibleText(billingUs.countrDdm,"Netherlands");
+      // Select select = new Select(billingUs.countrDdm);
+      // select.selectByVisibleText("Netherlands");
+
+       // Kullanici Street adress alanindaki House number street name alanina tiklar
+      //Kullanici house number ve street name bilgisini girer
+
+     //   action.doubleClick(billingUs.houseNstreetName);
+     //   billingUs.houseNstreetName.sendKeys("Balg 12");
+        action.moveToElement(billingUs.countrDdm).click().build().perform();
+        //Select dropdown = new Select(billingUs.countrDdm);
+
+       // action.moveToElement(billingUs.nederlandsddm).click().build().perform();
+        //dropdown.selectByVisibleText("Nederland");
+       // action.
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
