5bd02c81854febbde172dec2504daabaef66e13b
Mehmet Karaca <mehmetkaraca55@gmail.com>
2023-05-13 20:54:47 +0300

    v1

--- src/test/java/Team05/pages/VendorSignInLocates.java
+++ src/test/java/Team05/pages/VendorSignInLocates.java
@@ -1,5 +1,31 @@
 package Team05.pages;
 
+import Team05.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class VendorSignInLocates {
     //US_11
+    public VendorSignInLocates(){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+    @FindBy(xpath = "//*[text()='Sign In']")
+    public WebElement signInLogin;
+
+    @FindBy(xpath = "//*[@id='username']")
+    public WebElement email;
+
+    @FindBy(xpath = "//*[@id='password']")
+    public WebElement password;
+
+    @FindBy(xpath = "(//button[@type='submit'])[1]")
+    public WebElement signIn2;
+
+
+
+
+
+
+
 }
