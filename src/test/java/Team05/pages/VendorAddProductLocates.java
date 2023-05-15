10a19d453f2ef74a72908ca4b7a75f755c8e1f36
Zeynep Demir <zeynepnevadmr@gmail.com>
2023-05-15 21:06:26 +0300

    new

--- src/test/java/Team05/pages/VendorAddProductLocates.java
+++ src/test/java/Team05/pages/VendorAddProductLocates.java
@@ -204,30 +204,72 @@ public class VendorAddProductLocates {
         PageFactory.initElements(Driver.getDriver(), this);
 
     }
+    @FindBy(xpath = "//*[text()='Sign In']")
+    public WebElement login;
+    @FindBy(xpath="//*[@id='username']")
+    public WebElement userName;
 
-    //AddNew locator(zeynep)
+    @FindBy(xpath = "//*[@id='password']")
+    public WebElement password;
+
+    @FindBy(xpath = "(//*[text()='Sign In'])[2]")
+    public WebElement signin;
+
+
+   // @FindBy(xpath= "//li[@id='menu-item-1079']")
+   // public WebElement myAccount;
+
+
+    @FindBy(linkText = "My Account")
+    public static WebElement myAccount;
+
+    @FindBy(linkText = "Store Manager")
+    public static WebElement storeManager;
+
+
+    @FindBy(xpath= "//*[@class='wcfm_menu_items wcfm_menu_wcfm-products']")
+    public WebElement products;
+
     @FindBy(xpath = "(//*[text()='Add New'])[1]")
     public WebElement addNew;
 
+    @FindBy(xpath = "//*[@id='product_type']")
+    public WebElement simpleProduct;
+
+
     //Virtual tıklama(zeynep)
-    @FindBy(xpath = "//*[@id='is_virtual']")
+    @FindBy(css="input[id='is_virtual']")
     public WebElement virtual;
 
     //is_downloadable tıklama(zeynep)
-    @FindBy(xpath = "//*[@id='is_downloadable']")
+    @FindBy(css = "input[id='is_downloadable']")
     public WebElement is_downloadable;
+
+    @FindBy(css = "input[id='pro_title']")
+    public WebElement productTitle;
+
 
     //price kutucugu (zeynep)
     @FindBy(xpath = "//*[@id='regular_price']")
     public WebElement price;
 
     //sale price kutucugu(zeynep)
-    @FindBy(xpath = "//*[@id='sale_price']")
-    public WebElement salePrice;
+  //  @FindBy(xpath = "//*[@id='sale_price']")
+  //  public WebElement salePrice;
+
+
+
+    @FindBy(css = "input[id='wcfm_products_simple_draft_button']")
+    public WebElement draft;
+
+
+
+
+
 
     //variableProduct secimi(zeynep)
-    @FindBy(xpath = "//*[@id='product_type']")
-    public WebElement variableProduct;
+   // @FindBy(xpath = "//*[@id='product_type']")
+   // public static WebElement variableProduct;
 
     //color secimi(zeynep)
     @FindBy(xpath = "//*[@id='is_virtual']")
@@ -249,6 +291,36 @@ public class VendorAddProductLocates {
     @FindBy(xpath = "//*[@data-name='default_attribute_pa_size']")
     public WebElement anySize;
 
+
+
+
+
+
+
+
+
+
+    @FindBy(xpath="//img[@id='featured_img_display']")
+    public WebElement imageBuyuk;
+
+
+    @FindBy(xpath = "//img[@id='gallery_img_gimage_0_display']")
+    public WebElement imageKucuk;
+
+    @FindBy(xpath = "(//*[@*='228'])[2]")
+    public WebElement homeGarden;
+
+    @FindBy(xpath = "//input[@*='252']")
+    public WebElement homeDesign;
+
+    @FindBy(xpath = "(//input[@*='submit'])[3]")
+    public WebElement submitButton;
+
+    @FindBy(xpath = "//input[@value='View']")
+    public WebElement viewButton;
+
+    @FindBy(xpath = "(//img[@*='presentation'])[1]")
+    public WebElement productLastImage;
 
 }
 
