1b87508491076e8c6168daf15956f10696a0dae6
Zeynep Demir <zeynepnevadmr@gmail.com>
2023-05-17 02:41:09 +0300

    v5

--- src/test/java/Team05/pages/VendorAddProductLocates.java
+++ src/test/java/Team05/pages/VendorAddProductLocates.java
@@ -33,17 +33,23 @@ public class VendorAddProductLocates {
     @FindBy(xpath = "//*[@id='sale_price']")
     public WebElement salePrice;
 
-    //variableProduct secimi(zeynep)
-    @FindBy(xpath = "//*[@id='product_type']")
-    public WebElement variableProduct;
+
 
     //color secimi(zeynep)
-    @FindBy(xpath = "//*[@id='is_virtual']")
+    @FindBy(css = "input[id='attributes_is_active_1']")
     public WebElement color;
 
-    //size secimi(zeynep)
-    @FindBy(xpath = "//*[@id='attributes_is_active_2']")
+    @FindBy(xpath = "(//*[@class='select2-selection__rendered'])[2]")
+    public WebElement colorBox;
+
+
+    @FindBy(css = "input[id='attributes_is_active_2']")
     public WebElement size;
+
+    @FindBy(xpath = "(//*[@class='select2-selection__rendered'])[2]")
+    public WebElement sizeBox;
+
+
 
     // Variations'a tıkla(zeynep)
     @FindBy(xpath = "(//*[@class='page_collapsible_content_holder'])[6]")
@@ -58,8 +64,8 @@ public class VendorAddProductLocates {
     public WebElement anySize;
 
 
-//Buraya kadar Tuba
-
+    @FindBy(xpath = "//*[@id='wcfm_products_manage_form_attribute_head']")
+    public WebElement attributes;
 
     @FindBy(xpath = "//*[text()='Sign In']")
     public WebElement login;
@@ -72,9 +78,6 @@ public class VendorAddProductLocates {
     @FindBy(xpath = "(//*[text()='Sign In'])[2]")
     public WebElement signin;
 
-
-// @FindBy(xpath= "//li[@id='menu-item-1079']")
-// public WebElement myAccount;
 
 
     @FindBy(linkText = "My Account")
@@ -93,31 +96,20 @@ public class VendorAddProductLocates {
     @FindBy(css = "input[id='pro_title']")
     public WebElement productTitle;
 
-//sale price kutucugu(zeynep)
-//  @FindBy(xpath = "//*[@id='sale_price']")
-//  public WebElement salePrice;
-
 
     @FindBy(css = "input[id='wcfm_products_simple_draft_button']")
     public WebElement draft;
 
 
-//variableProduct secimi(zeynep)
-// @FindBy(xpath = "//*[@id='product_type']")
-// public static WebElement variableProduct;
+    //variableProduct secimi(zeynep)
+    @FindBy(xpath = "//*[@id='product_type']")
+    public static WebElement variableProduct;
 
 
     @FindBy(xpath = "//img[@id='featured_img_display']")
     public WebElement imageBuyuk;
 
-    @FindBy(xpath = "//img[@id='gallery_img_gimage_0_display']")
-    public WebElement imageKucuk;
 
-    @FindBy(xpath = "(//*[@*='228'])[2]")
-    public WebElement homeGarden;
-
-    @FindBy(xpath = "//input[@*='252']")
-    public WebElement homeDesign;
 
     @FindBy(xpath = "(//input[@*='submit'])[3]")
     public WebElement submitButton;
@@ -134,11 +126,6 @@ public class VendorAddProductLocates {
     @FindBy(xpath = "(//h2)[1]")
     public WebElement storeManagerText;
 
-    @FindBy(css = "a[class='wcfm_menu_item active']")
-    public WebElement products2;
-
-    @FindBy(xpath = "//*[text()='Products']")
-    public WebElement productsText;
 
     //DropDown
     @FindBy(xpath = "//*[@id='product_type']")
@@ -146,63 +133,7 @@ public class VendorAddProductLocates {
 }
 
 
-//buraya kadar zeynep
-//package Team05.pages;
-//
-//import Team05.utilities.Driver;
-//import org.openqa.selenium.WebElement;
-//import org.openqa.selenium.support.FindBy;
-//import org.openqa.selenium.support.PageFactory;
-//
-//public class VendorAddProductLocates {
-//    //US_14_18
-//    public VendorAddProductLocates() {
-//        PageFactory.initElements(Driver.getDriver(), this);
-//
-//    }
-//    //AddNew locator(zeynep)
-//    @FindBy(xpath = "(//*[text()='Add New'])[1]")
-//    public WebElement addNew;
-//
-//    //Virtual tıklama(zeynep)
-//    @FindBy(xpath = "//*[@id='is_virtual']")
-//    public WebElement virtual;
-//
-//    //is_downloadable tıklama(zeynep)
-//    @FindBy(xpath = "//*[@id='is_downloadable']")
-//    public WebElement is_downloadable;
-//
-//    //price kutucugu (zeynep)
-//    @FindBy(xpath = "//*[@id='regular_price']")
-//    public WebElement price;
-//
-//    //sale price kutucugu(zeynep)
-//    @FindBy(xpath = "//*[@id='sale_price']")
-//    public WebElement salePrice;
-//
-//    //variableProduct secimi(zeynep)
-//    @FindBy(xpath = "//*[@id='product_type']")
-//    public WebElement variableProduct;
-//
-//    //color secimi(zeynep)
-//    @FindBy(xpath = "//*[@id='is_virtual']")
-//    public WebElement color;
-//
-//    //size secimi(zeynep)
-//    @FindBy(xpath = "//*[@id='attributes_is_active_2']")
-//    public WebElement size;
-//
-//    // Variations'a tıkla(zeynep)
-//    @FindBy(xpath = "(//*[@class='page_collapsible_content_holder'])[6]")
-//    public WebElement variations;
-//
-//    // Any Color tıkla(zeynep)
-//    @FindBy(xpath = "//*[@data-name='default_attribute_pa_color']")
-//    public WebElement anyColor;
-//
-//    // Any size tıkla(zeynep)
-//   // @FindBy(xpath = "//*[@data-name='default_attribute_pa_size']")
-//    //public WebElement anySize;
+
 //
 //
 //
