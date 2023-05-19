1e6045c3068a6386f58b0d49502ca32a76a79640
Zeynep Demir <zeynepnevadmr@gmail.com>
2023-05-19 22:04:15 +0300

    son

--- src/test/java/Team05/test/US_16.java
+++ src/test/java/Team05/test/US_16.java
@@ -8,13 +8,14 @@ import Team05.utilities.ReusableMethods;
 import org.openqa.selenium.*;
 import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.security.Key;
 
 public class US_16 {
 
-           /*
+    /*
 Kullanici url'e gider
 Kullanici sign in butonuna tiklar
 Kullanici gecerli email ve password girer
@@ -29,10 +30,10 @@ Price ve Sale Price yazılabilmeli
 US14 ve US15 deki işlemler yapılabilmeli
 Ürünün eklendiğini Product kısmında görebilmeli
 
-            */
-           WebDriver driver;
-           Actions actions;
-           VendorAddProductLocates vendorAddProductLocates;
+     */
+    WebDriver driver;
+    Actions actions;
+    VendorAddProductLocates vendorAddProductLocates;
 
     @Test
     public void test1() {
@@ -52,7 +53,7 @@ US14 ve US15 deki işlemler yapılabilmeli
         vendorAddProductLocates.password.sendKeys(ConfigReader.getProperty("Vendor_Password"), Keys.ENTER);
 
 
-       // Kullanici my account yazisini goruntuler
+        // Kullanici my account yazisini goruntuler
 
         ReusableMethods.bekle(5);
         ReusableMethods.scrollEnd();
@@ -61,42 +62,39 @@ US14 ve US15 deki işlemler yapılabilmeli
         vendorAddProductLocates.myAccount.click();
 
 
-
-
-       // Kullanici store manager menusune tiklar
+        // Kullanici store manager menusune tiklar
         ReusableMethods.bekle(2);
         vendorAddProductLocates.storeManager.click();
 
 
-       // Kullanici store manager sayfasina gider
+        // Kullanici store manager sayfasina gider
         Assert.assertTrue(vendorAddProductLocates.storeManager.isDisplayed());
 
 
-       // Kullanici products menusune tiklar
+        // Kullanici products menusune tiklar
 
         ReusableMethods.bekle(2);
-        actions =new Actions(Driver.getDriver());
+        actions = new Actions(Driver.getDriver());
         actions.sendKeys(Keys.PAGE_DOWN).perform();
         ReusableMethods.bekle(2);
         Driver.getDriver();
         ReusableMethods.click(vendorAddProductLocates.products);
 
 
-       //Kullanıcı Add New butonuna tıklar
+        //Kullanıcı Add New butonuna tıklar
         ReusableMethods.bekle(2);
         vendorAddProductLocates.addNew.click();
 
 
-
-       // Simple Product default olarak gelmeli
+        // Simple Product default olarak gelmeli
         ReusableMethods.tumSayfaResmi();
         ReusableMethods.bekle(2);
         Assert.assertTrue(vendorAddProductLocates.simpleProduct.isDisplayed());
 
 
         // Virtual ve Downloadable seçilebilmeli
-        actions =new Actions(Driver.getDriver());
-        actions.sendKeys(Keys.ARROW_DOWN,Keys.ARROW_DOWN).perform();
+        actions = new Actions(Driver.getDriver());
+        actions.sendKeys(Keys.ARROW_DOWN, Keys.ARROW_DOWN, Keys.PAGE_DOWN).perform();
         vendorAddProductLocates.virtual.click();
         vendorAddProductLocates.is_downloadable.click();
 
@@ -104,28 +102,29 @@ US14 ve US15 deki işlemler yapılabilmeli
         vendorAddProductLocates.productTitle.sendKeys("Canta");
 
 
-
-       //Price ve Sale Price yazılabilmeli
-
-        ReusableMethods.bekle(2);
-        vendorAddProductLocates.price.sendKeys("100",Keys.TAB,"50");
-
-
-
-
-
-      // US14 ve US15 deki işlemler yapılabilmeli
+        //Price ve Sale Price yazılabilmeli
 
         ReusableMethods.bekle(2);
-        actions =new Actions(Driver.getDriver());
-        actions.sendKeys(Keys.PAGE_DOWN, Keys.PAGE_DOWN,Keys.PAGE_DOWN,Keys.PAGE_DOWN).perform();
+        vendorAddProductLocates.price.sendKeys("100", Keys.TAB, "50");
+
+
+        // US14 ve US15 deki işlemler yapılabilmeli
+
+        ReusableMethods.bekle(2);
+        actions = new Actions(Driver.getDriver());
+        actions.sendKeys(Keys.PAGE_DOWN, Keys.PAGE_DOWN, Keys.PAGE_DOWN, Keys.PAGE_DOWN).perform();
         ReusableMethods.click(vendorAddProductLocates.draft);
 
-        ReusableMethods.tumSayfaResmi();
+        // ReusableMethods.tumSayfaResmi();
 
 
-      // Ürünün eklendiğini Product kısmında görebilmeli
-        ReusableMethods.tumSayfaResmi();
+        // Ürünün eklendiğini Product kısmında görebilmeli
+        actions.sendKeys(Keys.PAGE_DOWN, Keys.PAGE_DOWN).perform();
+        ReusableMethods.click(vendorAddProductLocates.products);
+        ReusableMethods.bekle(3);
+        actions.sendKeys(Keys.PAGE_DOWN, Keys.PAGE_DOWN).perform();
+
+        //  ReusableMethods.tumSayfaResmi();
 
     }
 }
