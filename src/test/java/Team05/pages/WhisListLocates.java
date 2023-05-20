a8568aa13e5be3108cc8374f8b7c27c56371189e
Ali <alznky@gmail.com>
2023-05-20 21:46:11 +0300

    v3

--- src/test/java/Team05/pages/WhisListLocates.java
+++
@@ -1,5 +0,0 @@
-package Team05.pages;
-
-public class WhisListLocates {
-    //US_08
-}
--- src/test/java/Team05/test/US_08.java
+++ src/test/java/Team05/test/US_08.java
@@ -1,4 +1,53 @@
 package Team05.test;
 
-public class US_08 {
+import Team05.pages.WishListLocates;
+import Team05.utilities.ConfigReader;
+import Team05.utilities.Driver;
+import Team05.utilities.ReusableMethods;
+import org.openqa.selenium.Keys;
+import org.testng.annotations.Test;
+
+public class US_08 extends ReusableMethods {
+    @Test
+    public void wishList() {
+        //Kullanici anasayfaya gider
+        Driver.getDriver().get(ConfigReader.getProperty("Hubcomfy_Url"));
+        WishListLocates wishListLocates = new WishListLocates();
+
+        //Kullanici siteye giris yapar
+        wishListLocates.signInButon.click();
+        bekle(2);
+
+        wishListLocates.userName.sendKeys(ConfigReader.getProperty("Customer_Username"), Keys.TAB);
+        wishListLocates.password.sendKeys(ConfigReader.getProperty("Customer_Password"), Keys.ENTER);
+        bekle(3);
+
+        //Kulanici "elektronics" kategorisine tiklar
+        wishListLocates.electronics.click();
+        bekle(2);
+
+        //Kullanici urunu secer
+        wishListLocates.productClick.click();
+        bekle(2);
+
+        //Kullanici begendigi urunu "Wish Listesi" ne ekler
+        wishListLocates.addWishList.click();
+        bekle(3);
+
+        //Kullanici "Wish List" sekmesine gider
+        wishListLocates.goToWishList.click();
+        bekle(3);
+
+        //Kullanici "Wish List" e ekledigi urunu "Sepete Ek" ler
+        wishListLocates.addToCard.click();
+        bekle(2);
+
+        //Kullanici "Sepete" gider
+        wishListLocates.goToCard.click();
+        bekle(2);
+
+        //Kullanici odeme sayfasina gider
+        wishListLocates.goToPayment.click();
+        bekle(2);
+    }
 }
