2e41fe945370484863b15f283e18b8ada9eebe35
Fatih MIRIK <119960308+fmirik@users.noreply.github.com>
2023-05-21 15:27:27 +0300

    Update VendorAddProductLocates.java

--- src/test/java/Team05/pages/VendorAddProductLocates.java
+++ src/test/java/Team05/pages/VendorAddProductLocates.java
@@ -13,6 +13,163 @@ public class VendorAddProductLocates {
     }
 
     //zeynep baslangic
+    @FindBy(xpath = "(//*[text()='Add New'])[1]")
+    public WebElement addNew;
+    @FindBy(css = "input[id='attributes_is_active_1']")
+    public WebElement color;
+    @FindBy(css = "input[id='is_downloadable']")
+    public WebElement is_downloadable;
+    @FindBy(css = "input[id='attributes_is_active_2']")
+    public WebElement size;
+
+    @FindBy(xpath = "//*[@id='attributes_value_2']")
+    public WebElement sizeBox;
+    @FindBy(xpath = "//*[@id='wcfm_products_manage_form_variations_head']")
+    public WebElement variations;
+    @FindBy(xpath = "//*[@id='regular_price']")
+    public WebElement price;
+    @FindBy(xpath = "(//div[@class='thumbnail'])[3]")
+    public WebElement siyahAyakkabi1;
+    @FindBy(id = "featured_img_display")
+    public WebElement buyukProductImageEkleme;
+    @FindBy(xpath = "//*[text()='Sign In']")
+    public WebElement login;
+    @FindBy(xpath = "//*[@id='username']")
+    public WebElement userName;
+    @FindBy(xpath = "//*[@class='wcfm_menu_items wcfm_menu_wcfm-products']")
+    public WebElement products;
+    @FindBy(xpath = "//*[@id='product_type']")
+    public WebElement simpleProduct;
+    @FindBy(css = "input[id='pro_title']")
+    public WebElement productTitle;
+    @FindBy(xpath = "//*[@class='button wcfm_add_attribute_term wcfm_add_attributes_new_term']")
+    public WebElement addNewColor;
+    @FindBy(css = "input[id='wcfm_products_simple_draft_button']")
+    public WebElement draft;
+
+    @FindBy(xpath = "//span[text()='Sign In ']")
+    public WebElement signIn;
+
+    @FindBy(xpath = "(//input[@type='search'])[1]")
+    public WebElement searchBox;
+    @FindBy(css = "input[id='is_virtual']")
+    public WebElement virtual;
+    @FindBy(xpath = "//a[text()='Store Manager']")
+    public WebElement storeManager;
+
+    @FindBy(xpath = "(//a[text()='My Account'])[1]")
+    public WebElement myAccount;
+    @FindBy(xpath = "//select[@name='default_attributes[attribute_pa_color]']")
+    public WebElement variationsColor;
+    @FindBy(xpath = "//select[@name='variations[0][attribute_pa_color]']")
+    public WebElement variationAnyColor;
+    @FindBy(id = "variations_regular_price_0")
+    public WebElement regularPrice;
+    @FindBy(id = "variations_stock_status_0")
+    public WebElement stockStatus;
+    @FindBy(id = "variations_weight_0")
+    public WebElement weight;
+    @FindBy(id = "variations_length_0")
+    public WebElement length;
+    @FindBy(id = "variations_width_0")
+    public WebElement width;
+    @FindBy(id = "variations_height_0")
+    public WebElement height;
+    @FindBy(id = "variations_tax_class_0")
+    public WebElement tax;
+    @FindBy(id = "variations_description_0")
+    public WebElement descriptionVari;
+    @FindBy(xpath = "//button[text()='Media Library']")
+    public WebElement mediaLibrary;
+    @FindBy(xpath = "(//button[text()='Add New'])[2]")
+    public WebElement addNewSizeButton;
+
+    @FindBy(xpath = "//*[@class='button media-button button-primary button-large media-button-select']")
+    public WebElement imageSelectButton;
+
+    @FindBy(xpath = "//input[@value='229']")
+    public WebElement categoriesCheckBox;
+
+    @FindBy(xpath = "//input[@value='417']")
+    public WebElement productBrandscheckBox;
+
+    @FindBy(xpath = "//textarea[@id='product_tags']")
+    public WebElement productTags;
+
+    @FindBy(xpath = "//select[@id='catalog_visibility']")
+    public WebElement catalogvisibility;
+
+    @FindBy(xpath = "//label[@class='wcfmfa fa-server']")
+    public WebElement attributes;
+
+    @FindBy(xpath = "//input[@value='Submit']")
+    public WebElement submitProduct;
+
+
+    @FindBy(xpath = "//*[text()='Downloads'])[1]")
+    public WebElement downloads;
+
+    @FindBy(xpath = "//*[text()='Sign In']")
+    public WebElement signInLogin;
+
+    @FindBy(xpath = "//*[@id='username']")
+    public WebElement email;
+
+    @FindBy(xpath = "//*[@id='password']")
+    public WebElement password;
+
+
+    @FindBy(xpath = "//*[@class='page-title']")
+    public WebElement myAccount2;
+
+    @FindBy(xpath = "//*[text()='Add Product']")
+    public WebElement addNewText;
+    @FindBy(xpath = "//*[@id='product_type']")
+    public WebElement dropDown;
+
+
+    @FindBy(xpath = "//*[@id='product_url']")
+    public WebElement URL0;
+
+
+    @FindBy(xpath = "//*[@data-name='default_attribute_pa_color']")
+    public WebElement dropDown2;
+
+    @FindBy(xpath = "//*[@data-name='default_attribute_pa_size']")
+    public WebElement dropDown3;
+
+    @FindBy(xpath = "//*[@id='product_type']")
+    public static WebElement variableProduct;
+
+
+    @FindBy(xpath = "//*[@data-name='default_attribute_pa_color']")
+    public WebElement anyColor;
+
+    @FindBy(xpath = "//*[@data-name='default_attribute_pa_size']")
+    public WebElement anySize;
+
+    @FindBy(xpath = "//img[@id='featured_img_display']")
+    public WebElement imageBuyuk;
+
+
+    @FindBy(xpath = "//img[@id='gallery_img_gimage_0_display']")
+    public WebElement imageKucuk;
+
+    @FindBy(xpath = "(//*[@*='228'])[2]")
+    public WebElement homeGarden;
+
+    @FindBy(xpath = "//input[@*='252']")
+    public WebElement homeDesign;
+
+    @FindBy(xpath = "(//input[@*='submit'])[3]")
+    public WebElement submitButton;
+
+    @FindBy(xpath = "//input[@value='View']")
+    public WebElement viewButton;
+
+    @FindBy(xpath = "(//img[@*='presentation'])[1]")
+    public WebElement productLastImage;
+
 
     // zeynep son//
 
